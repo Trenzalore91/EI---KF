48b0c94d8d9cb9311744627a0b2bf18a27024820
Trenzalore91 <farina.alessandro@hotmail.com>
2025-03-21 20:34:16 +0100

    Markdown + lib

--- CLASS.py
+++ CLASS.py
@@ -1,13 +1,13 @@
 class FILTRE:
-    def __init__(self, fc, fs, ordre, type_filtre):
+    def __init__(self, id, fc, fs, ordre, type_filtre):
+        self.id = id
         self.fc = fc
         self.fs = fs
         self.ordre = ordre
         self.type_filtre = type_filtre
 
-    def __calculfiltre__(self, data):
-        nyquist = 0.5 * self.fs  #fréquence Nyquist
-        normal_cutoff = self.fc / nyquist    #fréquence de coupure normalisée
-        b, a = butter(self.ordre, normal_cutoff, btype=self.type_filtre)  #calcul des coefficients du filtre
-        y = lfilter(b, a, data) #application du filtre
-        return y
+    def __calculfiltre__(self, signal_a_filtrer):
+        freq_nyquist = 0.5 * self.fs
+        normal_freq_coupure = self.fc / freq_nyquist
+        b, a = butter(self.ordre, normal_freq_coupure, btype=self.type_filtre, analog=False)
+        y = lfilter(b, a, self.signal_a_filtrer)
