bde4279768a91342024a24333f2ac085ab6958f7
Trenzalore91 <farina.alessandro@hotmail.com>
2025-03-27 21:33:35 +0100

    objet

--- CLASS.py
+++ CLASS.py
@@ -7,16 +7,16 @@ class Filtre():
         self.signal_a_filtrer = signal_a_filtrer
         self.__calculfiltre__()
 
-class Filtres_standard(Filtre):
-    def __init__(self, type_filtre):
-        super().__init__(type_filtre)
-        
     def __calculfiltre__(self):
         freq = 0.5 * self.fs
         normal_freq_coupure = self.fc / freq
         b, a = butter(self.ordre, normal_freq_coupure, btype=self.type_filtre, analog=False)
         return lfilter(b, a, self.signal_a_filtrer)
-    
+
+class Filtres_standard(Filtre):
+    def __init__(self, type_filtre):
+        super().__init__(type_filtre)
+        
 class Filtre_Kalman():
     def __init__(self, signal_a_filtrer):
         self.signal_a_filtrer = signal_a_filtrer
