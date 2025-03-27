328bdd42522d5444e9c383417ae99435376d9009
Trenzalore91 <farina.alessandro@hotmail.com>
2025-03-27 21:05:15 +0100

    Git update

--- CLASS.py
+++ CLASS.py
@@ -1,13 +1,15 @@
 class FILTRE():
-    def __init__(self, id, fc, fs, ordre, type_filtre):
+    def __init__(self, id, fc, fs, ordre, type_filtre, signal_a_filtrer):
         self.id = id
         self.fc = fc
         self.fs = fs
         self.ordre = ordre
         self.type_filtre = type_filtre
+        self.signal_a_filtrer = signal_a_filtrer
+        self.__calculfiltre__()
 
-    def __calculfiltre__(self, signal_a_filtrer):
+    def __calculfiltre__(self):
         freq_nyquist = 0.5 * self.fs
         normal_freq_coupure = self.fc / freq_nyquist
         b, a = butter(self.ordre, normal_freq_coupure, btype=self.type_filtre, analog=False)
-        y = lfilter(b, a, self.signal_a_filtrer)
+        return lfilter(b, a, self.signal_a_filtrer)
