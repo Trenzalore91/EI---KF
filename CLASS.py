a4935be3d991991ca4b30a5479d5209d2f0a49ba
Trenzalore91 <farina.alessandro@hotmail.com>
2025-03-27 21:30:51 +0100

    update objet

--- CLASS.py
+++ CLASS.py
@@ -1,15 +1,23 @@
-class FILTRE():
-    def __init__(self, id, fc, fs, ordre, type_filtre, signal_a_filtrer):
+class Filtre():
+    def __init__(self, id, fc, fs, ordre, signal_a_filtrer):
         self.id = id
         self.fc = fc
         self.fs = fs
         self.ordre = ordre
-        self.type_filtre = type_filtre
         self.signal_a_filtrer = signal_a_filtrer
         self.__calculfiltre__()
 
+class Filtres_standard(Filtre):
+    def __init__(self, type_filtre):
+        super().__init__(type_filtre)
+        
     def __calculfiltre__(self):
-        freq_nyquist = 0.5 * self.fs
-        normal_freq_coupure = self.fc / freq_nyquist
+        freq = 0.5 * self.fs
+        normal_freq_coupure = self.fc / freq
         b, a = butter(self.ordre, normal_freq_coupure, btype=self.type_filtre, analog=False)
         return lfilter(b, a, self.signal_a_filtrer)
+    
+class Filtre_Kalman():
+    def __init__(self, signal_a_filtrer):
+        self.signal_a_filtrer = signal_a_filtrer
+    
