bf8202c8ae79b91e9485f94e3b7f586d0033e6dc
Trenzalore91 <farina.alessandro@hotmail.com>
2025-10-06 19:54:47 +0200

    Double anotation for 330 THz

--- CLASS.py
+++ CLASS.py
@@ -109,6 +109,9 @@ def zoom_graph(x, y, xlim, ylim, title, xlabel, ylabel):
         plt.annotate(f'{max_x:.2f} THz', xy=(max_x, max_y), xytext=(max_x, max_y + 0.1*max_y),
                     arrowprops=dict(facecolor='black', shrink=0.05),
                     horizontalalignment='center')
+        plt.annotate(f'330 THz', xy=(330, max_y), xytext=(330, max_y + 0.1*max_y),
+                    arrowprops=dict(facecolor='black', shrink=0.05),
+                    horizontalalignment='center')
     plt.grid(True)
     plt.show()
 
